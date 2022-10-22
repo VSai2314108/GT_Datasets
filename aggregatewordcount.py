b1c88108362838061eccdf0ae679453ccc52971f
vsai2314108 <somasundaramv@ufl.edu>
2022-10-22 18:58:46 -0400

    adding demo data

---
+++ aggregatewordcount.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import random
+def aggregatewc(b,e):
+    df = pd.read_csv('datasetwords.csv')
+    labels = df.columns[15:]
+    df = df[b:e]
+    df = df.sum(axis = 0)
+    df = df[15:]
+    # out = df.values.tolist()
+    df = df.to_list()
+    out = []
+    for ind,elem in enumerate(df):
+        if elem != 0:
+            add = {}
+            add['text'] = labels[ind]
+            add['value'] =elem+(elem*(random.random()))
+            out.append(add)
+    return out
+        
+        
+        
+    
+
+print(aggregatewc(0,90)) 
+    
+    
